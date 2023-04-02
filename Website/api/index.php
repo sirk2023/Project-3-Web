4fef4155fba670672f391befaa17881f297c9c9f
sirk2023 <a00287789@student.ait.ie>
2023-04-02 17:21:18 +0100

    Update
    Login System works correctly, Retrieve data on user whom is logged in

--- Website/api/index.php
+++ Website/api/index.php
@@ -1,26 +1,26 @@
 <?php
 require 'Slim/Slim.php';
-require 'wallet_db.php';
+require 'functions.inc.php';
 require 'database.php';
 use Slim\Slim;
 \Slim\Slim::registerAutoloader();
 
-//$app = new Slim();
-//  Wallet Methods
-//$app->get('/wallets', 'getWallets');
-//$app->get('/wallets/:id',  'getWallet');
-//$app->get('/wallets/search/:query', 'findByToken');
-//$app->post('/wallets', 'addWallet');
-//$app->delete('/wallets/:id',	'deleteToken');
-//$app->put('/wallets/:id', 'updateWallet');
+$app = new Slim();
+//  jobs Methods
+$app->get('/jobs', 'getJobs');
+$app->get('/jobs/:id',  'getJob');
+//$app->get('/jobs/search/:query', 'findByName');
+//$app->post('/jobs', 'addJob');
+//$app->delete('/jobs/:id',	'deleteJob');
+//$app->put('/jobs/:id', 'updateJob');
 
 //  User Methods
-$app->get('/users', 'getUsers');
-$app->get('/users/:id',  'getUser');
-$app->get('/users/search/:query', 'findByUser');
-$app->post('/users', 'addUser');
-$app->delete('/users/:id',	'deleteUser');
-$app->put('/users/:id', 'updateUser');
+//$app->get('/users', 'getUsers');
+//$app->get('/users/:id',  'getUser');
+//$app->get('/users/search/:query', 'findByUser');
+//$app->post('/users', 'addUser');
+//$app->delete('/users/:id',	'deleteUser');
+//$app->put('/users/:id', 'updateUser');
 $app->run();
 ?>
 
--- Website/profile.php
+++ Website/profile.php
@@ -3,12 +3,40 @@
 ?>
 
         <section class="index-intro">
-            <h1>This is an Introduction</h1>
-            <p>Here is an important stuff that the company does</p>
+        <h1>Welcome! <?php
+                echo $_SESSION['username']; 
+                ?></h1>
+            <p>Your ID is: <?php
+                echo $_SESSION['userid']; 
+                ?><br> Your Username is: <?php
+                echo $_SESSION['useruid']; 
+                ?><br> Your Email is: <?php
+                echo $_SESSION['useremail']; 
+                ?><br> You Logged in succesfully!
+            </p>
         </section>
         <section class="index-catagories">
             <div class="profile-left-panel">
-                <!-- content for left panel goes here -->
+                <div class="modal fade" id="myModal" role="dialog">
+                    <div class="modal-dialog">
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <!--Header Contents-->
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Token Details</h4>
+                            </div>
+                            <div class="modal-body" id="contents">
+                                <!--Body Contents-->
+                            </div>
+                            <div class="modal-footer" id="modalFooter">
+                                <!--Footer Contents-->
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <button id="addJob">Request Job</button>
             </div>
             <div class="profile-right-panel">
                 <!-- content for right panel goes here -->
