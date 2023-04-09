ebfe5c2a8f9382a1dab4f95a341129a4bdb4061e
JackWilliamOHara <a00286924@student.ait.ie>
2023-04-09 16:00:59 +0100

    Sorted Tables
    All tables now have the sorting function working

---
+++ Website1018/KillMyData.sql
@@ -0,0 +1,190 @@
+DROP DATABASE IF EXISTS KillMyData;
+CREATE DATABASE IF NOT EXISTS KillMyData;
+USE KillMyData;
+-- ------------------------------------------------------
+-- Table structure for table `user_table`
+--
+DROP TABLE IF EXISTS `user_table`;
+CREATE TABLE `user_table` (
+  `user_id`         int(11)       NOT NULL AUTO_INCREMENT,
+  `user_name`       varchar(45)   DEFAULT NULL,
+  `user_email`      varchar(45)   DEFAULT NULL,
+  `user_phone`      varchar(50)   DEFAULT NULL,
+  `user_uid`        varchar(45)   DEFAULT NULL,
+  `user_role`       varchar(45)   DEFAULT NULL,
+  `user_pass`       varchar(100)  DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+);
+INSERT INTO `user_table` VALUES
+(1, "Kyle",   "Kyle101@gmail.com", "0875555555", "Kyle1",   "Admin",  PASSWORD("pass")),
+(2, "Scot",   "Scot101@gmail.com", "0874444444", "Scot2",   "Admin",  PASSWORD("pass")),
+(3, "Stan",   "Stan183@gmail.com", "0873333333", "Scot3",   "Admin",  PASSWORD("pass"));
+
+-- ------------------------------------------------------
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customer_table`;
+CREATE TABLE `customer_table` (
+  `customer_id`   int(11)       NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(256)  DEFAULT NULL,
+  `customer_email` varchar(256) DEFAULT NULL,
+  `customer_number` int(45)     DEFAULT NULL,
+  `accounts_link`   varchar(100)  DEFAULT NULL,
+  `crm_link`        varchar(100)  DEFAULT NULL,
+  `user_id`         int(11)      NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`)
+);
+
+INSERT INTO `customer_table` VALUES
+(1, "Kyle",    "John@gmail.com",    "0871234567",  "Payment Link",   "CRM Managment",1),
+(2, "Scot",    "Jane@gmail.com",    "1234567890",  "Payment Link",   "CRM Managment",2),
+(3, "Stan",   "Alice@gmail.com",   "9876543215",  "Payment Link",   "CRM Managment",3);
+
+-- ------------------------------------------------------
+-- Table structure for table `request_job`
+--
+
+DROP TABLE IF EXISTS `request_job`;
+CREATE TABLE `request_job` (
+  `request_id`             	int(11)          NOT NULL AUTO_INCREMENT,
+  `customer_id`            	int(11)          NOT NULL,
+  `user_id`       			int(11)          NOT NULL,
+  `company_name`           	varchar(256)     DEFAULT NULL,
+  `user_phone`      		varchar(50)   	DEFAULT NULL,
+  `user_address`         	varchar(256)     DEFAULT NULL,
+  `additional_information` 	varchar(256)     DEFAULT NULL,
+  PRIMARY KEY (`request_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user_table`(`user_id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `customer_table`(`customer_id`)
+);
+
+INSERT INTO `request_job` VALUES
+(1, 1, 1, "PepeInc", "0877777777", "Pepe Road", "Only have 1 HDD"),
+(2, 2, 2, "PepeInc", "0866666666", "Pepe Road", "Only have 2 HDD"),
+(3, 3, 3, "PepeInc", "0855555555", "Pepe Road", "Only have 3 HDD");
+
+
+-- ------------------------------------------------------
+-- Table structure for table `accepted_jobs`
+--
+DROP TABLE IF EXISTS `accepted_jobs`;
+CREATE TABLE `accepted_jobs` (
+  `job_id` 						int(11)				NOT NULL AUTO_INCREMENT,
+  `customer_id` 				int(11)				NOT NULL,
+  `job_number` 					varchar(100) 		NOT NULL,
+  `job_status` 					varchar(45) 		DEFAULT NULL,
+  `job_creation_date` 			datetime 			DEFAULT NULL,
+  PRIMARY KEY (`job_id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `customer_table`(`customer_id`)
+);
+
+INSERT INTO `accepted_jobs` VALUES
+(1, 1,		"1A",	"Pending",		"2002-02-10"),
+(2, 2,		"1A",	"Pending",		"2002-02-10"),
+(3, 3,		"1A",	"Pending",		"2002-02-10");
+
+-- ------------------------------------------------------
+-- Table structure for table `Signature Table`
+--
+
+DROP TABLE IF EXISTS `signature_table`;
+CREATE TABLE `signature_table` (
+`signature_id` 				int(11) 		NOT NULL AUTO_INCREMENT,
+`job_id` 					int(11) 		NOT NULL,
+`customer_id` 				int(11) 		NOT NULL,
+`signature_date` 			datetime 		DEFAULT NULL,
+PRIMARY KEY (`signature_id`),
+FOREIGN KEY (`job_id`) REFERENCES `accepted_jobs`(`job_id`),
+FOREIGN KEY (`customer_id`) REFERENCES `customer_table`(`customer_id`)
+);
+INSERT INTO `signature_table` VALUES
+(1, 1,	1,	"2023-03-03"),
+(2, 2,	2,	"2023-03-04"),
+(3, 3,	3,	"2023-03-04");
+
+-- ------------------------------------------------------
+-- Table structure for table `batch_table`
+--
+DROP TABLE IF EXISTS `batch_table`;
+CREATE TABLE `batch_table` (
+  `batch_id` 				int(11)			AUTO_INCREMENT NOT NULL,
+  `job_id` 					int(11)			NOT NULL,
+  `batch_number`			varchar(100) 	DEFAULT NULL,
+  `seal_barcode_number`		varchar(100) 	DEFAULT NULL,
+  `batch_creation_date`		datetime 		DEFAULT NULL,
+  PRIMARY KEY (`batch_id`),
+  FOREIGN KEY (`job_id`) REFERENCES `accepted_jobs`(`job_id`)
+);
+
+INSERT INTO `batch_table` VALUES
+(1, 1,		"65451651",	"65451651",	"2023-04-03"),
+(2, 2,		"45616515",	"65451651",	"2023-04-04"),
+(3, 3,		"82848264",	"65451651",	"2023-04-04");
+
+-- ------------------------------------------------------
+-- Table structure for table `collected_harddrives`
+--
+
+DROP TABLE IF EXISTS `collected_harddrives`;
+CREATE TABLE `collected_harddrives` (
+	`collected_harddrives_id` 		int(11)			AUTO_INCREMENT NOT NULL,
+	`hdd_serial_number` 			varchar(100)	DEFAULT NULL,
+	`batch_id`						int(11) 		NOT NULL,
+	`signed_by_customer` 			VARCHAR(100)	DEFAULT NULL,
+	`signed_date` 					date 			DEFAULT NULL,
+  PRIMARY KEY (`collected_harddrives_id`),
+  UNIQUE KEY (`hdd_serial_number`),
+  FOREIGN KEY (`batch_id`) REFERENCES `batch_table`(`batch_id`)
+);
+INSERT INTO `collected_harddrives` VALUES
+(1, "4156465",		1,	"Yes",	"2023-03-03"),
+(2, "4156665",		2,	"Yes",	"2023-03-04"),
+(3, "4156365",		3,	"Yes",	"2023-03-04");
+
+-- ------------------------------------------------------
+-- Table structure for table `video_File_table`
+--
+DROP TABLE IF EXISTS `video_file_table`;
+CREATE TABLE `video_file_table` (
+`video_id` 				int(11) 		NOT NULL AUTO_INCREMENT,
+`job_id` 				int(11) 		NOT NULL,
+`batch_id` 				int(11) 		NOT NULL,
+`hdd_serial_number` 	varchar(100) 	NOT NULL,
+`capture_date` 			datetime 		DEFAULT NULL,
+`file_link` 			varchar(100) 	DEFAULT NULL,
+PRIMARY KEY (`video_id`),
+FOREIGN KEY (`job_id`) REFERENCES `accepted_jobs`(`job_id`),
+FOREIGN KEY (`batch_id`) REFERENCES `batch_table`(`batch_id`),
+FOREIGN KEY (`hdd_serial_number`) REFERENCES `collected_harddrives`(`hdd_serial_number`)
+);
+INSERT INTO `video_file_table` VALUES
+(1,1,1, "4156465","2023-03-15","Link"),
+(2,2,1, "4156465","2023-03-15","Link"),
+(3,3,1, "4156465","2023-03-15","Link");
+
+
+-- ------------------------------------------------------
+-- Table structure for table `completed_jobs`
+--
+
+DROP TABLE IF EXISTS `completed_jobs`;
+CREATE TABLE `completed_jobs` (
+  `job_id` 						int(11)			NOT NULL AUTO_INCREMENT,
+  `client_name` 				varchar(45)		DEFAULT NULL,
+  `date_of_request` 			date 			DEFAULT NULL,
+  `date_of_collection` 			date 			DEFAULT NULL,
+  `date_of_destruction` 		date 			DEFAULT NULL,
+  `certificate_of_destruction` 	varchar(256) 	DEFAULT NULL,
+  `status` 						varchar(256) 	DEFAULT NULL,
+  `video_link` 					varchar(256) 	DEFAULT NULL,
+  `client_id` 					int(45) 		DEFAULT NULL,
+  `crm_link` 					varchar(256) 	DEFAULT NULL,
+  PRIMARY KEY (`job_id`)
+);
+
+INSERT INTO `completed_jobs` VALUES
+(1, "Kyle",		"2002-02-10",	"2002-02-10",		"2002-02-10",	"destructionlink",		"Complete",		"videolink", 	"accID",	"CRMlink"),
+(2, "Kyle",		"2002-02-10",	"2002-02-10",		"2002-02-10",	"destructionlink",		"Complete",		"videolink", 	"accID",	"CRMlink"),
+(3, "Kyle",		"2002-02-10",	"2002-02-10",		"2002-02-10",	"destructionlink",		"Complete",		"videolink", 	"accID",	"CRMlink");
---
+++ Website1018/api/index.php
@@ -0,0 +1,83 @@
+<?php
+require 'Slim/Slim.php';
+require 'functions.inc.php';
+require 'database.php';
+use Slim\Slim;
+\Slim\Slim::registerAutoloader();
+
+$app = new Slim();
+//  request_job Methods
+$app->get('/requestedJobs', 'getRequestedJobs');
+$app->get('/requestedJobs/:id',  'getRequestedJob');
+$app->get('/requestedJobs/search/:query', 'findByCompanyName');
+$app->post('/requestedJobs', 'requestAddJob');
+$app->delete('/requestedJobs/:id',	'deleteRequestedJob');
+$app->put('/requestedJobs/:id', 'updateRequestedJob');
+
+//  User Table Methods
+$app->get('/users', 'getUsers');
+$app->get('/users/:id',  'getUser');
+//$app->get('/users/search/:query', 'findByUser');
+//$app->post('/users', 'addUser');
+//$app->delete('/users/:id',	'deleteUser');
+//$app->put('/users/:id', 'updateUser');
+
+//  Customers Table Methods
+$app->get('/customers', 'getCustomers');
+$app->get('/customers/:id',  'getCustomer');
+$app->get('/customers/search/:query', 'findByCustomerName');
+$app->post('/customers', 'addCustomer');
+$app->delete('/customers/:id',	'deleteCustomer');
+//$app->put('/customers/:id', 'updateCustomer');
+
+//  Accepted Job Table Methods
+$app->get('/acceptedJobs', 'getAcceptedJobs');
+$app->get('/acceptedJobs/:id',  'getAcceptedJob');
+$app->post('/acceptedJobs', 'addAcceptedJob');
+$app->delete('/acceptedJobs/:id',	'deleteAcceptedJob');
+//$app->put('/acceptedJobs/:id', 'updateAcceptedJob');
+
+// Job Table
+$app->get('/alljobs', 'getJobsA');
+$app->get('/alljobs/:id',  'getJobA');
+
+//  Signature Table Methods
+$app->get('/signatures', 'getSignatures');
+$app->get('/signatures/:id',  'getSignature');
+$app->post('/signatures', 'addSignature');
+$app->delete('/signatures/:id',	'deleteSignature');
+//$app->put('/signatures/:id', 'updateSignature');
+
+// Batch Table
+$app->get('/batches', 'getBatches');
+$app->get('/batches/:id',  'getBatch');
+$app->post('/batches', 'addBatch');
+$app->delete('/batches/:id',	'deleteBatch');
+//$app->put('/batches/:id', 'updateBatch');
+
+//  Collected Hard Drives Table Methods
+$app->get('/collectedHardDrives', 'getCollectedHardDrives');
+$app->get('/collectedHardDrives/:id',  'getCollectedHardDrive');
+$app->post('/collectedHardDrives', 'addCollectedHardDrive');
+$app->delete('/collectedHardDrives/:id',	'deleteCollectedHardDrive');
+//$app->put('/collectedHardDrives/:id', 'updateCollectedHardDrive');
+
+//  Video File Table Methods
+$app->get('/videoFiles', 'getVideoFiles');
+$app->get('/videoFiles/:id',  'getVideoFile');
+$app->post('/videoFiles', 'addVideoFile');
+$app->delete('/videoFiles/:id',	'deleteVideoFile');
+//$app->put('/videoFiles/:id', 'updateVideoFile');
+
+//  Completed Jobs Table Methods
+$app->get('/completedJobs', 'getCompletedJobs');
+$app->get('/completedJobs/:id',  'getCompletedJob');
+$app->get('/completedJobs/search/:query', 'findByCompletedJobName');
+$app->post('/completedJobs', 'addCompletedJob');
+$app->delete('/completedJobs/:id',	'deleteCompletedJob');
+//$app->put('/completedJobs/:id', 'updateCompletedJob');
+$app->run();
+?>
+
+
+
